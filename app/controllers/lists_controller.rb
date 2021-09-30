53f115447527b173ccc56e0822f0c5dc238d13fb
Emilie LeMaoult <emilie.lemaoult@gmail.com>
2021-09-30 20:42:48 +0200

    Bundler fix

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show]
+  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
@@ -21,6 +21,11 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to root_path
+  end
+
   private
 
   def set_list
@@ -28,6 +33,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
